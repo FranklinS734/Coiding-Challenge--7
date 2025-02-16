17d2eeffbdab0e08a032c6397e7209dccef1681b
FranklinS734 <martinc3@usf.edu>
2025-02-15 23:32:32 -0500

    Task 5 - Business Loan Interest Calculation

--- cc_7.js
+++ cc_7.js
@@ -54,3 +54,14 @@ function calculateShippingCost(weight, location, expedited = false) {
 // Test Data
 console.log(calculateShippingCost(10, "USA", true)); // Expected: "Shipping Cost: $20.00"
 console.log(calculateShippingCost(5, "Canada", false)); // Expected: "Shipping Cost: $13.50"
+
+// Task 5: Returning Values
+function calculateLoanInterest(principal, rate, years) {
+    // Calculate total interest using formula given 
+    let interest = principal * rate * years;
+    return `Total Interest: $${interest.toFixed(2)}`;
+}
+
+// Test Data
+console.log(calculateLoanInterest(1000, 0.05, 3)); // Expected: "Total Interest: $150.00"
+console.log(calculateLoanInterest(5000, 0.07, 5)); // Expected: "Total Interest: $1750.00"
